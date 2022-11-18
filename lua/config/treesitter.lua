4ffc426a30d23e917de13f1b896ab2581520a2b9
Henk Houwaard <henkhouwaard@gmail.com>
2022-11-18 23:33:57 +0100

    10 completion (#10)

---
+++ lua/config/treesitter.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+function M.setup()
+  require("nvim-treesitter.configs").setup {
+    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = { "lua", "c_sharp", "css", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "html", "http", "javascript", "json", "vue", "typescript" },
+
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    highlight = {
+      -- `false` will disable the whole extension
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+
+    indent = { enable = true },
+  }
+end
+
+return M
